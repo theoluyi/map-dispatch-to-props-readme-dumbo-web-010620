9b0690f4ef4b4f966e34ae13e7929f9f3a1502a5
github username <github email address>
2020-07-20 14:13:46 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    // this.props.store.dispatch(addItem()); 
+    // this.props.dispatch(addItem());  // not quite
+    this.props.addItem(); 
   }
 
   render() {
+    debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -27,4 +30,19 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = dispatch => {
+//   // this is weird... we are returning an object with the key addItem
+//   // pointing to the return value of an anon ƒ invoking dispatch with addItem() as its arg
+//   // this is a lotta object + function nesting
+
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   }
+// }
+
+
+// map state to props basically allows the component to 
+// 'inherit' app state from redux store as props locally
+export default connect(mapStateToProps, {addItem})(App);
